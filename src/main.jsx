a6df3e669c052e70bdef80a8df21ec4965701121
So1968 <sofia.rios@gmx.fr>
2026-05-29 11:33:52 +0200

    Affiche le Log Pose dans Vogue Merry

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { LogPoseOverlay } from "./LogPoseOverlay.jsx";
 import "./demo-stabilisation.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <LogPoseOverlay />
+  </>
+);
