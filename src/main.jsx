1181341ca940af2a3522f6ffd886a236909f5974
So1968 <sofia.rios@gmx.fr>
2026-07-31 17:36:10 +0200

    Retire les anciennes couches visuelles du demarrage

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./demo-stabilisation.css";
-import "./vogue-art-direction-reset.css";
 
 createRoot(document.getElementById("root")).render(<App />);
