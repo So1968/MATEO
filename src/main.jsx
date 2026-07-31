deff6e84c0ea634418fb0165d265ebbf38fb6349
So1968 <sofia.rios@gmx.fr>
2026-07-31 17:28:12 +0200

    Branche le reset de direction artistique

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./demo-stabilisation.css";
+import "./vogue-art-direction-reset.css";
 
 createRoot(document.getElementById("root")).render(<App />);
