c8478ce1b75e2bcd1ca122ba5e2dc7768dfbad4c
So1968 <sofia.rios@gmx.fr>
2026-07-31 20:32:34 +0200

    Restaure la stabilisation avec le Log Pose

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { LogPoseOverlay } from "./LogPoseOverlay.jsx";
+import "./demo-stabilisation.css";
 
 createRoot(document.getElementById("root")).render(
   <>
