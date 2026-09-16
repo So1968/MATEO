db4677b46293816343ee3ba5c0a1cc4f589a0d7c
So1968 <sofia.rios@gmx.fr>
2026-09-16 22:35:51 +0200

    Branche la transcription locale dans Vogue Marry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import TranscriptionView from "./TranscriptionView.jsx";
 import "./demo-stabilisation.css";
 import "./carte-mentale-aventure.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const pathname = window.location.pathname.replace(/\/+$/, "") || "/";
+const root = createRoot(document.getElementById("root"));
+
+root.render(pathname === "/transcription" ? <TranscriptionView /> : <App />);
