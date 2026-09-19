114d0ed4523c407e95f544cfad11e55731811d02
So1968 <sofia.rios@gmx.fr>
2026-09-19 16:02:02 +0200

    Branche la synchronisation des interlocuteurs

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import TranscriptionView from "./TranscriptionView.jsx";
+import "./speaker-map-sync.js";
 import "./demo-stabilisation.css";
 import "./carte-mentale-aventure.css";
 
